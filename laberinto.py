f6215a521bcff7e6c35b4ccd10ec8981a7d702f4
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-24 16:21:25 +0100

    Añadida la entrada y la salida al código

--- laberinto.py
+++ laberinto.py
@@ -1,5 +1,7 @@
 # El laberinto tiene una dimensión 5x5
 muros = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+entrada=(0,0)
+salida=(5,5)
 def laberinto (dim,muros):
     laberinto = []
     # coordenada1= i
@@ -7,10 +9,14 @@ def laberinto (dim,muros):
     for i in range(dim):
         fila = []
         for j in range(dim):
-            if tuple([i, j]) in muros:
-                fila.append('X')
-            else:
-                fila.append('  ')
+            if tuple == entrada:
+                fila.append('E')
+            if tuple == salida:
+                fila.append('S')
+                if tuple([i, j]) in muros:
+                    fila.append('X')
+                else:
+                    fila.append('  ')
         laberinto.append(fila)
     return laberinto
 
