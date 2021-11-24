2962bd331197bc0dfb7dc07c961d10367d6f5223
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-24 15:55:15 +0100

    creado el bucle que añadirá las filas y planteado el poner los muros

--- laberinto.py
+++ laberinto.py
@@ -3,4 +3,7 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 
 def laberinto ():
     Dibujolaberinto = []
-    fila = []
+    for coordenada1 in range():
+        fila = []
+        if tuple([coordenada1, coordenada2]) in muro:
+            
