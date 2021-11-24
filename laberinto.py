43b1083810def9d36579335670c4f14de67f0c83
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-24 16:01:33 +0100

    creada la función del laberinto y cambio en el nombre de las coordenadas de coordenadas1 y 2 a i y j como si fueran coordenadas del plano cartesiano

--- laberinto.py
+++ laberinto.py
@@ -2,8 +2,16 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 
 def laberinto ():
-    Dibujolaberinto = []
-    for coordenada1 in range():
+    global dibujolaberinto
+    dibujolaberinto = []
+    coordenada1="i"
+    coordenada2="j"
+    for i in range:
         fila = []
+    for j in range:
         if tuple([coordenada1, coordenada2]) in muro:
-            
+            fila.append('X')
+        else:
+            fila.append('  ')
+        laberinto.append(fila)
+    return laberinto
