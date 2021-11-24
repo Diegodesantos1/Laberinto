dbb38c290040fd7f11aee5f2d0871a933f4a2664
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-24 16:09:25 +0100

    cambio de nombre de variables, añadida la dimensión del laberinto del enunciado, la función está basada en la dimensión y los muros y falta printear el laberinto y arreglar pequeños errores

--- laberinto.py
+++ laberinto.py
@@ -1,17 +1,17 @@
-
+# El laberinto tiene una dimensión 5x5
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-
-def laberinto ():
-    global dibujolaberinto
-    dibujolaberinto = []
+dim=5
+def laberinto (dim,muros):
+    laberinto = []
     coordenada1="i"
     coordenada2="j"
-    for i in range:
+    for i in range(dim):
         fila = []
-    for j in range:
+    for j in range(dim):
         if tuple([coordenada1, coordenada2]) in muro:
             fila.append('X')
         else:
             fila.append('  ')
-        laberinto.append(fila)
+    laberinto.append(fila)
     return laberinto
+
