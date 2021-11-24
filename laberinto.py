f2af3f36999e0e90dc86e516fa75617c946704a2
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-24 16:49:52 +0100

    arreglados los espacios, pero sigue sin printear la entrada y la salida

--- laberinto.py
+++ laberinto.py
@@ -1,7 +1,7 @@
 # El laberinto tiene una dimensión 5x5
 muros = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-entrada= ((0,0))
-salida= ((5,5))
+entrada= (0,0)
+salida= (5,5)
 def laberinto (dim,muros):
     laberinto = []
     # coordenada1= i
@@ -9,18 +9,18 @@ def laberinto (dim,muros):
     for i in range(dim):
         fila = []
         for j in range(dim):
-            if tuple([i,j]) in entrada:
+            if tuple ([i,j]) in entrada:
                 fila.append('E')
-            elif tuple([i,j]) in salida:
+            if tuple([i,j]) in salida:
                 fila.append('S')
-            elif tuple([i, j]) in muros:
+            if tuple([i,j]) in muros:
                 fila.append('X')
             else:
-                fila.append('  ')
+                fila.append(' ')
         laberinto.append(fila)
     return laberinto
 
 dibujolaberinto= laberinto(5,muros)
 
 for i in dibujolaberinto:
-    print(' '.join(i))
+    print(''.join(i))
