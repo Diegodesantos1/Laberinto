bf67b963b91e66e45c694acd70b5272ec46a4de5
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-11-24 16:14:40 +0100

    Printea el laberinto pero no en columnas, sino en una sola línea, además arreglado el nombre de la variable muros y ejecutada la función en base a 5 que es la dimensión y los muros dados

--- laberinto.py
+++ laberinto.py
@@ -1,5 +1,5 @@
 # El laberinto tiene una dimensión 5x5
-muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+muros = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 dim=5
 def laberinto (dim,muros):
     laberinto = []
@@ -8,9 +8,13 @@ def laberinto (dim,muros):
     for i in range(dim):
         fila = []
         for j in range(dim):
-            if tuple([coordenada1, coordenada2]) in muro:
+            if tuple([coordenada1, coordenada2]) in muros:
                 fila.append('X')
             else:
                 fila.append('  ')
         laberinto.append(fila)
     return laberinto
+
+dibujolaberinto= laberinto(5,muros)
+
+print(dibujolaberinto)
